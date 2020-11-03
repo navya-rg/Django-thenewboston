790b2ac8c59fb28733c233699b09e2e8f046a820
navya <navyarg11@gmail.com>
2020-11-03 15:44:54 +0530

    end of 16th video - raising a 404 HTTP Error

--- music/views.py
+++ music/views.py
@@ -1,17 +1,22 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+#from django.http import HttpResponse
+from django.shortcuts import render
+#from django.template import loader
 from .models import Album
 
 # Create your views here.
 def index(request):
-     all_albums = Album.objects.all()
-     #By default Django looks in the templates directory
-     template = loader.get_template('music/index.html')
-     context = {
-        'all_albums' : all_albums,
-     }
-     return HttpResponse(template.render(context, request))
+    all_albums = Album.objects.all()
+    context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', context)
+
+     # all_albums = Album.objects.all()
+     # #By default Django looks in the templates directory
+     # template = loader.get_template('music/index.html')
+     # context = {'all_albums' : all_albums}
+     # return HttpResponse(template.render(context, request))
+
     # all_albums = Album.objects.all()
     # html = ''
     # for album in all_albums:
@@ -20,4 +25,9 @@ def index(request):
     # return HttpResponse(html)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk = album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
+    #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
