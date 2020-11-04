e8e6d26bba49104358470c49bc6ed911846f72e8
navya <navyarg11@gmail.com>
2020-11-04 13:52:25 +0530

    end of 24th video - favourite view function

--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@
 #from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 #from django.template import loader
-from .models import Album
+from .models import Album, Song
 
 # Create your views here.
 def index(request):
@@ -32,3 +32,14 @@ def detail(request, album_id):
     #     raise Http404("Album does not exist")
     return render(request, 'music/detail.html', {'album': album})
     #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk = album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message': 'You did not select a valid song'})
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
