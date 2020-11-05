e954b1d2ea93b7f5da76d661b414ed28d6b94770
navya <navyarg11@gmail.com>
2020-11-05 13:29:13 +0530

    end of 32nd video - updateview and deleteview

--- music/urls.py
+++ music/urls.py
@@ -13,6 +13,13 @@ urlpatterns = [
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
 
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
+
     # /music/<album_id>/favourite/
     #url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
 ]
