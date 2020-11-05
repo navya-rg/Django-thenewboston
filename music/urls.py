3e328cd9ece2a41cdb8f0012c6eef540bc27f032
navya <navyarg11@gmail.com>
2020-11-05 13:11:20 +0530

    end of 31st video - modelform and createview

--- music/urls.py
+++ music/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add/
+    url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
     # /music/<album_id>/favourite/
     #url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
 ]
